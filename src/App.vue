22b05550386ba8167698ecdf611b967b8c34a45d
Anton Tsyhan <anton.tsyhan@uco.co.il>
2024-12-11 20:22:53 +0300

    finish video 6

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,108 @@
-<template>
-    <input type="text" v-model="userName" placeholder="Имя" />
-    <input type="password" v-model="userPass" placeholder="Пароль" />
-    <input type="email" v-model="userEmail" placeholder="Email" />
-    <p className="error">{{ error }}</p>
-    <button @click="sendData()">Отправить</button>
-    <p>{{ users }}</p>
-
-    <div v-if="users.length == 0">У нас нет пользователей</div>
-    <div v-else-if="users.length == 1">User has 1 element</div>
-    <div v-else>Users has more 1 element</div>
-
-    <User v-for="(el, index) in users" :key="index" :user="el" :index="index" :deleteUser="deleteUser" />
-</template>
-
 <script>
-import User from './components/User.vue';
+import axios from 'axios';
 
 export default {
-    components: { User },
     data() {
         return {
-            users: [],
+            city: '',
             error: '',
-            userName: '',
-            userPass: '',
-            userEmail: '',
+            info: null,
         };
     },
+    computed: {
+        cityName() {
+            return '<' + this.city + '>';
+        },
+        showTemp() {
+            return 'Температура: ' + this.info.main.temp;
+        },
+    },
     methods: {
-        sendData() {
-            if (this.userName == '') {
-                this.error = 'Имя не введено';
-                return;
-            } else if (this.userEmail == '') {
-                this.error = 'Email не введен';
-                return;
-            } else if (this.userPass == '') {
-                this.error = 'Pass не введен';
-                return;
+        getWeather() {
+            if (this.city.trim().length < 2) {
+                this.error = 'Нужно название более одного символа :)';
+                return false;
             }
 
             this.error = '';
 
-            this.users.push({
-                name: this.userName,
-                pass: this.userPass,
-                email: this.userEmail,
-            });
-        },
-        methods: {
-            sendData() {},
-        },
-        deleteUser(index) {
-            this.users.splice(index, 1);
+            axios
+                .get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=b9cf0246f283dea29ee567a7db288c26`)
+                .then(res => (this.info = res.data));
         },
     },
 };
 </script>
 
-<style scoped></style>
+<template>
+    <div class="wrapper">
+        <h1>Погодное приложение</h1>
+        <p>Узнать погоду в {{ city == '' ? 'вашем городе' : cityName }}</p>
+        <input type="text" v-model="city" placeholder="Введите город" />
+        <button v-if="city != ''" @click="getWeather()">Получить погоду</button>
+        <button disabled v-else>Введите название города</button>
+        <p class="error">{{ error }}</p>
+        <div v-if="info != null">
+            <p>{{ showTemp }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.error {
+    color: red;
+}
+
+.wrapper {
+    width: 900px;
+    height: 500px;
+    border-radius: 50px;
+    padding: 20px;
+    background: black;
+    text-align: center;
+    color: white;
+}
+
+.wrapper h1 {
+    margin-top: 50px;
+}
+
+.wrapper p {
+    margin-top: 20px;
+}
+
+.wrapper input {
+    margin-top: 30px;
+    background: transparent;
+    border: 0;
+    border-bottom: 2px solid black;
+    color: white;
+    font-size: 14px;
+    padding: 5px 8px;
+    outline: none;
+}
+
+.wrapper input:focus {
+    border-bottom-color: greenyellow;
+}
+
+.wrapper button {
+    background: rgb(240, 149, 2);
+    color: white;
+    border-radius: 10px;
+    border: 2px solid rgb(168, 228, 16);
+    padding: 10px 15px;
+    margin-left: 20px;
+    cursor: pointer;
+    transition: transform 500ms ease;
+}
+
+.wrapper button:disabled {
+    background: rgb(179, 107, 7);
+    cursor: not-allowed;
+}
+
+.wrapper button:hover {
+    transform: scale(1, 1) translateY(-5px);
+}
+</style>
